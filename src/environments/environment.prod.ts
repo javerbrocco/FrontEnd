e22f867e3bbf284d17ea556f0f0f96dc46ae0c7b
mariano <104591927+MoreNeme@users.noreply.github.com>
2023-09-21 16:13:30 -0300

    carga final

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,13 +1,13 @@
 export const environment = {
-  firebase: {
-    projectId: 'frontend-44d67',
+   firebase: {
+     projectId: 'frontend-44d67',
     appId: '1:660769284136:web:d9d2dea07374144549d51d',
     storageBucket: 'frontend-44d67.appspot.com',
     locationId: 'southamerica-east1',
     apiKey: 'AIzaSyDe1tX9oDnL4FNeXIpuzrzqMj85aLaweCU',
     authDomain: 'frontend-44d67.firebaseapp.com',
     messagingSenderId: '660769284136',
-  },
+   },
   production: true,
-  URL:'https://backend-production-a1d3.up.railway.app/'
+  URL:'https://bck-tzp0.onrender.com/'
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,19 +3,19 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  firebase: {
+   firebase: {
     projectId: 'frontend-44d67',
-    appId: '1:660769284136:web:d9d2dea07374144549d51d',
-    storageBucket: 'frontend-44d67.appspot.com',
-    locationId: 'southamerica-east1',
+     appId: '1:660769284136:web:d9d2dea07374144549d51d',
+     storageBucket: 'frontend-44d67.appspot.com',
+     locationId: 'southamerica-east1',
     apiKey: 'AIzaSyDe1tX9oDnL4FNeXIpuzrzqMj85aLaweCU',
-    authDomain: 'frontend-44d67.firebaseapp.com',
-    messagingSenderId: '660769284136',
-  },
+     authDomain: 'frontend-44d67.firebaseapp.com',
+     messagingSenderId: '660769284136',
+   },
   production: false,
   URL:'http://localhost:8080/',
-  //URL:'https://backend-production-e309.up.railway.app/'
-};
+ 
+}
 
 /*
  * For easier debugging in development mode, you can import the following file
